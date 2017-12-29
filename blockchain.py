0448713d03f4188fdea20d69ab0169a4b158aa57
yojkim <youthful2016@gmail.com>
2017-12-29 13:32:23 +0900

    initial commit

---
+++ blockchain.py
@@ -0,0 +1,22 @@
+class BlockChain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        # Creates a new block and adds it to the chain
+        pass
+
+    def new_transaction(self):
+        # Adds a new transactions to the list of transactions
+        pass
+
+    @staticmethod
+    def hash(block):
+        # Hashes a block
+        pass
+
+    @property
+    def last_block(self):
+        # Returns the last block in the chain
+        pass
