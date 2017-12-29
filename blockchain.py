434cb41b9ae29d6bd85e72771339a2454655035b
yojkim <youthful2016@gmail.com>
2017-12-29 17:22:43 +0900

    simple implementation of blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,22 +1,48 @@
+import hashlib
+import json
+from time import time
+
 class BlockChain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # Creates a new block and adds it to the chain
-        pass
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
 
-    def new_transaction(self):
-        # Adds a new transactions to the list of transactions
-        pass
+    # Creates a new block and adds it to the chain
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index':            len(self.chain) + 1,
+            'timestamp':        time(),
+            'transactions':     self.current_transactions,
+            'proof':            proof,
+            'previous_hash':    previous_hash or self.hash(self.chain[-1]),
+        }
+
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    # Adds a new transactions to the list of transactions
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender':       sender,
+            'recipient':    recipient,
+            'amount':       amount,
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
+    # Hashes a block
     def hash(block):
-        # Hashes a block
-        pass
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
+    # Returns the last block in the chain
     def last_block(self):
-        # Returns the last block in the chain
+        return self.chain[-1]
         pass
