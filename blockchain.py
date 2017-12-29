89b806cd6ad2d451b6b6520c2160ef40ff19a825
yojkim <youthful2016@gmail.com>
2017-12-29 17:30:45 +0900

    proof of work

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
+
 from time import time
+from uuid import uuid4
 
 class BlockChain(object):
     def __init__(self):
@@ -35,11 +37,24 @@ class BlockChain(object):
 
         return self.last_block['index'] + 1
 
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
     @staticmethod
     # Hashes a block
     def hash(block):
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
+
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        
+        return guess_hash[:4] == "0000"
 
     @property
     # Returns the last block in the chain
